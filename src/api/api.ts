2f7ed5cabab0e6d7377ca18fe46a7c8220ce4947
nevty <theacutemace@gmail.com>
2020-07-10 06:51:12 +0700

    интерфейс и типы для запроса поездок

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,5 +1,6 @@
 import axios from "axios"
-import {ErrorCodeEnum, LoginDataType} from "../types/types";
+import {ErrorCodeEnum, LoginDataType, TripType} from "../types/types";
+import {GetTokenType} from "../utils/sessionStorage";
 
 const baseURL = 'http://transstage1.iwayex.com/transnextgen';
 
@@ -9,14 +10,46 @@ const instance = axios.create({
 
 export const usersAPI = {
     login(data: LoginDataType) {
-        return instance.post('v3/auth/login', JSON.stringify(data), {
+        return instance.post<UserLoginResponseType>('v3/auth/login', JSON.stringify(data), {
             headers: {'content-type': 'application/json'}
         })
+            .then(response => response.data)
     },
 }
 
 export type UserLoginResponseType = {
     result: { token: string } | null,
+    error: {
+        name: string,
+        message: string,
+        code: number,
+        status: ErrorCodeEnum,
+        type: string
+    } | null
+}
+
+export const tripsAPI = {
+    getTrips(token:ReturnType<GetTokenType>) {
+        return instance.get<GetTripsResponseType>('v3/orders/trips',{
+            headers: {
+                'content-type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            }
+        })
+            .then(response => response.data)
+    }
+}
+
+export type GetTripsResponseType = {
+    result: {
+        orders: Array<TripType>,
+        page_data: {
+            page: number,
+            items_on_page: number,
+            total_items: number,
+            page_count: number
+        }
+    },
     error: {
         name: string,
         message: string,
--- src/types/types.ts
+++ src/types/types.ts
@@ -1,5 +1,6 @@
 export enum ErrorCodeEnum {
-    wrongAuthStatus = 400
+    wrongAuthStatus = 400,
+    unAuthorizedRequest = 401,
 }
 
 export type LoginDataType = {
@@ -7,6 +8,37 @@ export type LoginDataType = {
     password: string
 }
 
+type Passenger = {
+    name: string,
+    email: string,
+    phone?: string,
+    company?: string,
+    client_id?: number,
+    company_id?: number
+}
+
+export type TripType = {
+    passengers: Array<Passenger>
+    status: number,
+    location_address: string,
+    destination_address: string,
+    duration: number,
+    booker_number: string,
+    car_data: {
+        car_class_id: number,
+        car_class: string,
+        models: string,
+        capacity: number
+    },
+    price: {
+        price_id: number,
+        price: number,
+        price_subaddress: number
+    },
+}
+
 type PropertiesTypes<T> = T extends { [key: string]: infer U } ? U : never
+// Выводит типы свойств внутри объекта
 
 export type InferActionTypes<T extends { [key: string]: (...args: any[]) => any }> = ReturnType<PropertiesTypes<T>>
+// Выводит типы свойств внутри объекта которая вернула функция
