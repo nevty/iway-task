0f3a27f9f2516dba6c094d99bc663b2da7af7422
nevty <theacutemace@gmail.com>
2020-07-14 11:23:37 +0700

    маска для ввода телефона

--- src/Components/FormComponents/FormComponents.tsx
+++ src/Components/FormComponents/FormComponents.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {TextField, TextFieldProps} from "@material-ui/core";
+import InputMask from "react-input-mask";
 
 type OwnTextFieldPropsType = {
     value?: string
@@ -13,4 +14,17 @@ export const renderTextField: React.FC<TextFieldProps & OwnTextFieldPropsType> =
         value={value}
         {...custom}
     />
+)
+
+export const MaskedTextField: React.FC<TextFieldProps & OwnTextFieldPropsType> = ({placeholder, onChange, value, ...custom}) => (
+    <InputMask mask="+7 (999) 999-99-99" maskChar={null} value={value} onChange={onChange}>
+        {() => (
+            <TextField
+                {...custom}
+                placeholder={placeholder}
+                type="tel"
+                fullWidth={true}
+            />
+        )}
+    </InputMask>
 )
