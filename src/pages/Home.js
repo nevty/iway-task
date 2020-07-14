1e5035c7aa60e3b3038fa29f7230bc38d923ed17
nevty <theacutemace@gmail.com>
2020-07-14 11:25:51 +0700

    вынес обертку

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,15 @@
 import React from "react";
 import TripsContainer from "../Components/TripsContainer/TripsContainer";
+import Box from "@material-ui/core/Box";
 
 const Home = ({history}) => {
     const redirect = (route)=>{
         history.push(route)
     }
     return (
-        <div>
+        <Box component="section">
             <TripsContainer redirect={redirect}/>
-        </div>
+        </Box>
     )
 }
 
